15cc870519cfe40148573a67e3a71efa1f7e5880
Christian Ambukita <christianambukita@gmail.com>
2022-04-11 23:48:18 +0200

    Add multiClick sendProblem color mapping

--- src/index.js
+++ src/index.js
@@ -1,89 +1,98 @@
-'use strict';
+"use strict";
 
-const LOAD_PROBLEM = 'LOAD_PROBLEM';
-const ADD_PROBLEM = 'ADD_PROBLEM';
-const EDIT_PROBLEM = 'EDIT_PROBLEM';
-const ACTIVE_PROBLEM = 'ACTIVE_PROBLEM';
+const LOAD_PROBLEM = "LOAD_PROBLEM";
+const ADD_PROBLEM = "ADD_PROBLEM";
+const EDIT_PROBLEM = "EDIT_PROBLEM";
+const ACTIVE_PROBLEM = "ACTIVE_PROBLEM";
 const TOPIC = {
-	LOAD: 'load',
-	SAVE: 'save',
-	LOAD_STATE: 'loadState',
-	SAVE_STATE: 'saveState',
+  LOAD: "load",
+  SAVE: "save",
+  LOAD_STATE: "loadState",
+  SAVE_STATE: "saveState",
 };
 let doubleSliderInstance = undefined;
 const setInstance = (instance) => (doubleSliderInstance = instance);
 
 function mapGrade(index) {
-	return GRADES[index];
+  return GRADES[index];
+}
+
+function getDiodeColor(diodeIndex, colorState) {
+  if (!ENABLE_BOARD_DESCRIPTION) {
+    let diodeState = (gripState) => (gripState ? 255 : 0);
+    return `${diodeIndex},0,${diodeState(colorState)},0`;
+  }
+  let diodeColor = [0, 0, 0];
+  if (typeof colorState === "number") diodeColor = BOARD_CONFIG.multiTapColors[colorState];
+  return `${diodeIndex},${diodeColor.join(",")}`;
 }
 
 function sendProblem({ grips }) {
-	let diodeState = (gripState) => (gripState ? 255 : 0);
-	let msg = { topic: 'display' };
-	msg.payload = Object.keys(grips)
-		.map((key, index) => `${diodeIndexes[index]},0,${diodeState(grips[key])},0`)
-		.filter((elem, index) => diodeIndexes[index] !== undefined)
-		.join('|');
-	uibuilder.send(msg);
+  let msg = { topic: "display" };
+  msg.payload = Object.keys(grips)
+    .map((key, index) => getDiodeColor(diodeIndexes[index], grips[key]))
+    .filter((elem, index) => diodeIndexes[index] !== undefined)
+    .join("|");
+  uibuilder.send(msg);
 }
 
 function getBoardSize() {
-	let board = document.getElementById('board-container');
-	console.log(board);
+  let board = document.getElementById("board-container");
+  console.log(board);
 }
 
 const app1 = new Vue({
-	el: '#app',
+  el: "#app",
 
-	components: {
-		'add-problem': httpVueLoader('components/AddProblem.vue'),
-		'load-problem': httpVueLoader('components/LoadProblem.vue'),
-		'active-problem': httpVueLoader('components/ActiveProblem.vue'),
-		'app-component': httpVueLoader('components/AppComponent.vue'),
-		'menu-bar': httpVueLoader('components/Menu.vue'),
-	},
+  components: {
+    "add-problem": httpVueLoader("components/AddProblem.vue"),
+    "load-problem": httpVueLoader("components/LoadProblem.vue"),
+    "active-problem": httpVueLoader("components/ActiveProblem.vue"),
+    "app-component": httpVueLoader("components/AppComponent.vue"),
+    "menu-bar": httpVueLoader("components/Menu.vue"),
+  },
 
-	computed: {
-		isAddProblem() {
-			return this.$store.getters.getActiveScene === ADD_PROBLEM;
-		},
-		isLoadProblem() {
-			return this.$store.getters.getActiveScene === LOAD_PROBLEM;
-		},
-		isActiveProblem() {
-			return this.$store.getters.getActiveScene === ACTIVE_PROBLEM;
-		},
-	},
+  computed: {
+    isAddProblem() {
+      return this.$store.getters.getActiveScene === ADD_PROBLEM;
+    },
+    isLoadProblem() {
+      return this.$store.getters.getActiveScene === LOAD_PROBLEM;
+    },
+    isActiveProblem() {
+      return this.$store.getters.getActiveScene === ACTIVE_PROBLEM;
+    },
+  },
 
-	methods: {},
+  methods: {},
 
-	store,
-	mounted: function () {
-		uibuilder.start();
+  store,
+  mounted: function () {
+    uibuilder.start();
 
-		var vueApp = this;
-		uibuilder.send({ topic: TOPIC.LOAD_STATE });
-		uibuilder.send({ topic: TOPIC.LOAD });
+    var vueApp = this;
+    uibuilder.send({ topic: TOPIC.LOAD_STATE });
+    uibuilder.send({ topic: TOPIC.LOAD });
 
-		uibuilder.onChange('msg', function (msg) {
-			if (msg.topic === TOPIC.LOAD) {
-				if (msg.payload.length === 0) {
-					store.commit('setProblemList', []);
-					return console.warn('EMPTY PROBLEM LIST');
-				}
-				let newProblemList = JSON.parse(msg.payload);
-				console.log('loaded msg: ', newProblemList);
-				store.commit('setProblemList', newProblemList);
-			}
-			if (msg.topic === TOPIC.LOAD_STATE) {
-				//ActiveState
-				const loadedState = JSON.parse(msg.payload);
-				if (loadedState) {
-					//console.log(loadedState, Object.entries(loadedState).length);
-					store.commit('setActiveProblem', loadedState);
-				}
-				//console.log('loaded_state: ', loadedState);
-			}
-		});
-	},
+    uibuilder.onChange("msg", function (msg) {
+      if (msg.topic === TOPIC.LOAD) {
+        if (msg.payload.length === 0) {
+          store.commit("setProblemList", []);
+          return console.warn("EMPTY PROBLEM LIST");
+        }
+        let newProblemList = JSON.parse(msg.payload);
+        console.log("loaded msg: ", newProblemList);
+        store.commit("setProblemList", newProblemList);
+      }
+      if (msg.topic === TOPIC.LOAD_STATE) {
+        //ActiveState
+        const loadedState = JSON.parse(msg.payload);
+        if (loadedState) {
+          //console.log(loadedState, Object.entries(loadedState).length);
+          store.commit("setActiveProblem", loadedState);
+        }
+        //console.log('loaded_state: ', loadedState);
+      }
+    });
+  },
 });
